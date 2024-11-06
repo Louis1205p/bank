c5c40640383f43e4014d03265674bde4bf7f7b0b
Louis1205p <ltdpedersen@gmail.com>
2024-11-06 08:01:45 +0100

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Account
+    {
+        private decimal balance;
+        public Account(decimal initialDeposit)
+        {
+            if (initialDeposit < 100)
+            {
+                Console.WriteLine("must be at least 100.");
+                balance = 100;
+            }
+            else
+            {
+                balance = initialDeposit;
+            }
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("You must deposit a positive amount.");
+                return;
+            }
+            balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("You must withdraw a positive amount.");
+                return;
+            }
+            if (amount > balance)
+            {
+                Console.WriteLine("You must withdraw a number within you balance");
+                return;
+            }
+            balance -= amount;
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Balance: {balance:C}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿
+using Bank;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your first name to start creating your account:");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name:");
+        string lastName = Console.ReadLine();
+
+        Random rand = new Random();
+        int accountId = rand.Next(100000, 1000000);
+
+        Console.WriteLine($"Account Owner: {firstName} {lastName}");
+        Console.WriteLine($"Account ID: {accountId}");
+
+        Console.Write("Make your first deposit (minimum 100,-): ");
+        decimal initialDeposit = Convert.ToDecimal(Console.ReadLine());
+
+        Account myAccount = new Account(initialDeposit);
+
+        myAccount.ShowBalance();
+
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Deposit Money");
+            Console.WriteLine("2. Withdraw Money");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter the amount you wish to deposit: ");
+                    decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+                    myAccount.Deposit(depositAmount);
+                    break;
+
+                case "2":
+                    Console.Write("Enter the amount you wish to withdraw: ");
+                    decimal withdrawAmount = Convert.ToDecimal(Console.ReadLine());
+                    myAccount.Withdraw(withdrawAmount);
+                    break;
+
+                case "3":
+                    myAccount.ShowBalance();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Exiting");
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("try again.");
+                    break;
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
